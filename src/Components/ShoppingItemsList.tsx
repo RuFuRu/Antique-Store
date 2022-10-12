155c1f4216ebf58293395a45db1d3410f9d5993f
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-10-12 21:40:45 +0200

    list

--- src/Components/ShoppingItemsList.tsx
+++ src/Components/ShoppingItemsList.tsx
@@ -9,9 +9,11 @@ interface Props {
 function ShoppingItemsList(item: Props) {
     return (
         <div className="shoppingitemslist-container">
-            <img src={item.url} alt="item picture" />
-            <span>{item.name}</span>
-            <span>{item.price}</span>
+            <img src={`${item.url}`} alt="item picture" />
+            <div>
+                <span>{item.name}</span>
+                <span>{item.price}</span>
+            </div>
         </div>
     )
 }
