8cb81edf27e08afa460356d7f82d2e87b4680710
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-10-14 20:14:26 +0200

    added more items

--- src/Components/Filter.tsx
+++ src/Components/Filter.tsx
@@ -24,11 +24,11 @@ function Filter() {
             <div className="filter-filter-type-btn-container">
                 <button className="filter-type-button" name="firearm" onClick={(e) => (filterOut(e))}>Firearms</button>
                 <button className="filter-type-button" name="navigation" onClick={(e) => (filterOut(e))}>Navigation</button>
-                <button className="filter-type-button" name="reset-filter" onClick={(e) => (filterOut(e))}>Reset</button>
+                <button className="filter-type-button" name="clock" onClick={(e) => (filterOut(e))}>Clocks</button>
             </div>
             <hr id="filter-hr2"/>
             <div className="filter-filter-btn">
-                <button className="filter-button">Filter</button>
+                <button className="filter-button" name="reset-filter" onClick={(e) => (filterOut(e))}>Reset</button>
             </div>
         </div>
     )
