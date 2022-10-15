7e87d4e6747907daef4e91849bfee7b5d7490bc8
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-10-16 00:41:54 +0200

    wip

--- src/Pages/ShoppingCard.tsx
+++ src/Pages/ShoppingCard.tsx
@@ -1,6 +1,38 @@
+import { useContext } from 'react';
+import AntiqueStoreContext from '../AntiqueStoreContext';
+import Header from '../Components/Header';
+import Information from '../Components/Information';
+import '../Sass/pages/ShoppingCard.scss';
+import shopItems from '../Data/shopItems.json';
+
 function ShoppingCard() {
+    const context = useContext(AntiqueStoreContext);
+    const amount = context!.getOverallAmount();
+
     return (
-        <div className="shoppingcard-container"></div>
+        <div className="shoppingcard-container">
+            <Header/>
+            <Information/>
+            {amount === 0 &&
+                <>
+                    <div className="sc-no-amount-information">
+                        <p className="sc-no-amount-p">
+                            It seems you haven't added anything to cart yet.
+                        </p>
+                    </div>
+                </>
+            }
+            {amount > 0 &&
+            <>
+                <h3 className="sc-h3">Checkout</h3>
+                <div className="sc-items">
+                    {
+                    }
+                </div>
+            </>
+            }
+
+        </div>
     )
 }
 
