9c9f7bacd05b7567fe6e4f1c6ac1c4521471144e
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-10-15 20:19:15 +0200

    html page for displaying single product

--- src/Pages/Item.tsx
+++ src/Pages/Item.tsx
@@ -1,7 +1,37 @@
+import { url } from "inspector";
+import { useParams } from "react-router-dom";
+import Header from "../Components/Header";
+import Information from "../Components/Information";
+import shopItems from "../Data/shopItems.json";
+
+
 function Item() {
+    const params = useParams();
+    const urlId = parseInt(params.id!);
+
     return (
         <div className="item-page-container">
-            <h1>Hello there</h1>
+            <Header/>
+            <Information/>
+            <div className="second-item-container">
+                {shopItems.filter(item => item.id === urlId).map(item => (
+                    <>
+                        <div className="item-page-left-side">
+                            <img src={item.url} alt="product-image" />
+                        </div>
+                        <div className="item-page-right-side">
+                            <h3>{item.name}</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Reiciendis eaque nostrum aliquam dicta deserunt recusandae voluptates mollitia rerum quae cupiditate! 
+                                Quidem quo porro dolore enim, libero possimus accusantium nisi corporis.
+                            </p>
+                            <input type="number" />
+                            <button>Add to cart</button>
+                        </div>
+                    </>
+                ))}
+            </div>
         </div>
     )
 }
